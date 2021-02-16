fb7ede1f21533cfff27e39ae7a1a9b16c7cdd94e
RafaelKC <rafaelkaua97@gmail.com>
2021-02-15 23:30:12 -0300

    Modify api/tempo.js

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,9 +1,15 @@
-function tempo(request, response) {
-     const dynamicDate = new Date();
+async function tempo(request, response) {
+    const cepSecret = process.env.VIACEP_API_SECRET; 
+    const dynamicDate = new Date();
 
+    const cepResponse = await fetch(`http://viacep.com.br/ws/${cepSecret}/json/`);
+    const cepResponseJson = await cepResponse.json();
+    const bairro = cepResponseJson.bairro;
+ 
      response.json({
          date: dynamicDate.toGMTString(),
-     });
+        bairro: bairro,
+        });
 }
 
 export default tempo;
